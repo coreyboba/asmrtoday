1489e6b44d4fabe0ef8aa8e65287ae435f65a6ba
Corey Boba <coreyboba@gmail.com>
2015-08-22 18:51:21 -0400

    Site modifications made.

--- asmrtoday/asmrtoday/App_Start/BundleConfig.cs
+++ asmrtoday/asmrtoday/App_Start/BundleConfig.cs
@@ -21,8 +21,8 @@ namespace asmrtoday
 
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
                       "~/Scripts/bootstrap.js",
-                      "~/Scripts/site.js",
-                      "~/Scripts/respond.js"));
+                      "~/Scripts/respond.js",
+                      "~/Scripts/site.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
